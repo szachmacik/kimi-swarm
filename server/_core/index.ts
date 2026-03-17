6a92241b4a4e8c9aa8d65a1968b8a15596b84f3a
szachmacik <maciej.koziej01@gmail.com>
2026-03-17 08:51:11 +0100

    fix: port 0.0.0.0 + guardian order (auto)

--- server/_core/index.ts
+++ server/_core/index.ts
@@ -13,7 +13,7 @@ import { guardianRouter } from "../guardian";
 function isPortAvailable(port: number): Promise<boolean> {
   return new Promise(resolve => {
     const server = net.createServer();
-    server.listen(port, () => {
+    server.listen(port, "0.0.0.0", () => {
       server.close(() => resolve(true));
     });
     server.on("error", () => resolve(false));
